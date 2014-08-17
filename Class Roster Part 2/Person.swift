f746ea32dbdf24c4236385a2bf3b159b04bb2721
mbrightbill <brightbill.mt@gmail.com>
2014-08-17 11:14:54 -0700

    First Commit

---
+++ Class Roster Part 2/Person.swift
@@ -0,0 +1,22 @@
+//
+//  Person.swift
+//  Class Roster Part 2
+//
+//  Created by Matthew Brightbill on 8/17/14.
+//  Copyright (c) 2014 Matthew Brightbill. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// Step 1. Create person class with defined properties and init.
+class Person {
+    var firstName: String
+    var lastName: String
+    var image: UIImage?
+    
+    init(fName: String, lName: String) {
+        self.firstName = fName
+        self.lastName = lName
+    }
+}
